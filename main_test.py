63377d63da13a02b2533d6e1f098cb2636ef2c1b
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-11-07 03:56:07 +0900

    test: Add test_hiraganafy_todo

--- main_test.py
+++ main_test.py
@@ -5,12 +5,43 @@ import main
 class TestStringMethods(unittest.TestCase):
 
     def test_hiraganafy(self):
-        katakana = 'カタカナ'
-        self.assertEqual(main.hiraganafy(katakana), 'かたかな')
-        kanji = '漢字'
-        self.assertEqual(main.hiraganafy(kanji), 'かんじ')
-        hiragana = 'ひらがな'
-        self.assertEqual(main.hiraganafy(hiragana), 'ひらがな')
+        test_cases = [
+            {'in': 'カタカナ', 'out': 'かたかな'},
+            {'in': '漢字', 'out': 'かんじ'},
+            {'in': 'ひらがな', 'out': 'ひらがな'},
+            {'in': '哺乳瓶', 'out': 'ほにゅうびん'},
+            {'in': '長距離走', 'out': 'ちょうきょりそう'},
+            {'in': '営', 'out': 'えい'},
+            {'in': '営マーク', 'out': 'えいまーく'},
+            {'in': '営業中', 'out': 'えいぎょうちゅう'},
+            {'in': '営業日', 'out': 'えいぎょうび'},
+        ]
+        for t in test_cases:
+            self.assertEqual(main.hiraganafy(t['in']), t['out'])
+
+    def test_hiraganafy_todo(self):
+        test_cases = [
+            {'in': 'しかめ面', 'out': 'しかめっつら'},
+            {'in': 'しかめ面の人', 'out': 'しかめつらのひと'},
+            {'in': 'バルカン人', 'out': 'ばるかんじん'},
+            {'in': 'フェイスマッサージ中の人', 'out': 'ふぇいすまっさーじちゅうのひと'},
+            {'in': '水球をする人', 'out': 'すいきゅうをするひと'},
+            {'in': '人のシルエット', 'out': 'ひとのしるえっと'},
+            {'in': '話す人のシルエット', 'out': 'はなすひとのしるえっと'},
+            {'in': '2人のシルエット', 'out': 'ふたりのしるえっと'},
+            {'in': '六芒星', 'out': 'ろくぼうせい'},
+            {'in': '六角星', 'out': 'ろっかくせい'},
+            {'in': '波乗り', 'out': 'なみのり'},
+            {'in': 'きらきら星', 'out': 'きらきらぼし'},
+            {'in': 'くす玉', 'out': 'くすだま'},
+            {'in': 'ソロモン諸島', 'out': 'そろもんしょとう'},
+            {'in': 'クリッパートン島', 'out': 'くりっぱーとんとう'},
+            {'in': '白杖', 'out': 'はくじょう'},
+            {'in': '介助犬', 'out': 'かいじょけん'},
+            {'in': '筋トレ', 'out': 'きんとれ'},
+        ]
+        for t in test_cases:
+            self.assertNotEqual(main.hiraganafy(t['in']), t['out'])
 
     def test_add_word_to_dict(self):
         emoji_dict = []
