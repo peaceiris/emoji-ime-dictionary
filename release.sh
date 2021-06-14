10a3bacb3d5efc85a0c6945bfc1fb8bb6e6deb49
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2021-06-14 21:11:12 +0900

    ci: Improve workflows (#55)
    - Enable COMPOSE_DOCKER_CLI_BUILD DOCKER_BUILDKIT
    - Replace hub with gh
    
    The reason to use `gh release delete` is here: https://github.com/cli/cli/issues/1997

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ RELEASE_ASSETS_DIR="build"
 RELEASE_NOTES="release_notes.md"
 
 
-\rm -rf "${RELEASE_ASSETS_DIR}" || true
+rm -rf "${RELEASE_ASSETS_DIR}" || true
 mkdir "${RELEASE_ASSETS_DIR}"
 cat $(echo ./tsv/*.tsv) > "./${RELEASE_ASSETS_DIR}/emoji.txt"
 (
@@ -35,10 +35,7 @@ cat $(echo ./tsv/*.tsv) > "./${RELEASE_ASSETS_DIR}/emoji.txt"
 
 sed -i "1i日本語 IME 絵文字拡張辞書 ${TAG_NAME}\n" "./${RELEASE_NOTES}"
 
-(
-  cd "${RELEASE_ASSETS_DIR}"
-  hub release edit \
-    --file "../${RELEASE_NOTES}" \
-    $(for i in $(echo *); do echo -n "--attach ${i}#${i} "; done) \
-    "${TAG_NAME}"
-)
+gh release delete --yes "${TAG_NAME}"
+gh release create "${TAG_NAME}" \
+  --notes-file "./${RELEASE_NOTES}" \
+  "${RELEASE_ASSETS_DIR}"/*
